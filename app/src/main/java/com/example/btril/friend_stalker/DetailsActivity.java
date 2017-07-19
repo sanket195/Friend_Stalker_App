27ec7bbd721a48b48d952993c5a9516b7d71777e
ravikanthnookala <ravikanth.kf290@gmail.com>
2017-07-19 15:00:21 -0700

    added menu item

---
+++ app/src/main/java/com/example/btril/friend_stalker/DetailsActivity.java
@@ -0,0 +1,8 @@
+package com.example.btril.friend_stalker;
+
+/**
+ * Created by Ravi on 7/18/2017.
+ */
+
+class DetailsActivity {
+}
--- app/src/main/java/com/example/btril/friend_stalker/LoginActivity.java
+++ app/src/main/java/com/example/btril/friend_stalker/LoginActivity.java
@@ -3,32 +3,26 @@ package com.example.btril.friend_stalker;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -59,6 +53,32 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     // TODO (1) create the options menu for the main login page
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.home) {
+            startActivity(new Intent(this, DetailsActivity.class));
+            return true;
+        }
+
+        if (id == R.id.logout) {
+            startActivity(new Intent(this, LoginActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
