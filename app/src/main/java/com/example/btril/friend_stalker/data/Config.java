a7517484a1e94230c66fec511adde7287fd165ea
btgn <tbakula@calstatela.edu>
2017-07-29 01:55:36 -0700

    Added Config and Controller Tools for the application

---
+++ app/src/main/java/com/example/btril/friend_stalker/data/Config.java
@@ -0,0 +1,13 @@
+package com.example.btril.friend_stalker.data;
+
+/**
+ * Created by ritapatel on 7/29/17.
+ */
+
+public class Config {
+    public static String LOGIN_URL = "http://nearbyfriendsapplogin.gear.host/";
+    public static String REGISTER_URL = "http://nearbyfriendsapplogin.gear.host/";
+    public static String LOCATION_URL = "http://nearbyfriendsapplogin.gear.host/";
+    public static String FRIEND_URL = "http://nearbyfriendsapplogin.gear.host/";
+    public static String GETFRIENDS_URL = "http://nearbyfriendsapplogin.gear.host/include/get_friends.php";
+}
---
+++ app/src/main/java/com/example/btril/friend_stalker/data/Controller.java
@@ -0,0 +1,53 @@
+package com.example.btril.friend_stalker.data;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by ritapatel on 7/29/17.
+ */
+
+/*used to get and put request*/
+public class Controller extends Application {
+    public static final String LOG_TAG = Controller.class.getSimpleName();
+    private RequestQueue requestQueue;
+    private static Controller controller;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        controller = this;
+    }
+
+    public static synchronized Controller getController(){
+        return controller;
+    }
+
+    // gets the request
+    public RequestQueue getRequestQueue(){
+        if(requestQueue == null)
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        return requestQueue;
+    }
+
+    // adding the request
+    public <T> void addRequestQueue(Request<T> req) {
+        req.setTag(LOG_TAG);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addRequestQueue(Request<T> req, String tag) {
+        req.setTag(TextUtils.isEmpty(tag) ? LOG_TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPending(Object tag) {
+        if(requestQueue == null)
+            requestQueue.cancelAll(tag);
+    }
+
+}
