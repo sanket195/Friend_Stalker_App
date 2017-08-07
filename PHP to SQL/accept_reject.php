8d3b5e68134f7aa1ba5ad5713eb132f0ba5f5a09
juhi16792 <lrjuheeshwari16@gmail.com>
2017-08-07 15:22:27 -0700

    friends operations on database

---
+++ PHP to SQL/accept_reject.php
@@ -0,0 +1,26 @@
+<?php
+
+$servername = "mssql6.gear.host";
+$username = “friend_stalker”;
+$password = "Di78B?!Qu5ou";
+$dbname=“friend_stalker”;
+
+
+$user_email =$_GET['user_email'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT * FROM friends where friend_email = '".$user_email."' and is_friend='false' order by friends_id desc limit 1";
+$result1 = $conn->query($sql);
+   if ($result1->num_rows > 0) {
+        while($row1 = $result1->fetch_assoc()) {
+            $lat_long_array[] = $row1;
+        }
+    } else {
+        echo "0 results";
+    }
+
+echo json_encode($lat_long_array);
+?>
